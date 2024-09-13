49b5b33d701f629efeef5daf8329f608dc164515
Yash-2907 <guptayp_1@rknec.edu>
2024-09-13 17:55:58 +0530

    uii

--- app/src/main/java/com/example/silentwhisper/MainActivity.kt
+++ app/src/main/java/com/example/silentwhisper/MainActivity.kt
@@ -10,9 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.shift).setOnClickListener{
-            startActivity(Intent(this@MainActivity,RegisterPage::class.java))
-            finish()
-        }
     }
 }
