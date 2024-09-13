db4a52ad50fdf1918cbc213f627616c813230776
Anushree2023 <anushreethosar@gmail.com>
2024-09-13 23:01:18 +0530

    Activity Changer

--- app/src/main/java/com/example/silentwhisper/LogIn.kt
+++ app/src/main/java/com/example/silentwhisper/LogIn.kt
@@ -1,11 +1,31 @@
 package com.example.silentwhisper
 
+import android.content.Intent
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.silentwhisper.databinding.ActivityLogInBinding
+
+
+lateinit var binder:ActivityLogInBinding
 
 class LogIn : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        binder=ActivityLogInBinding.inflate(layoutInflater)
+        setContentView(binder.root)
+
+        binder.submitbtn.setOnClickListener {
+
+
+        }
+
+
+        binder.newuserbtn.setOnClickListener {
+            startActivity(Intent(this@LogIn,RegisterPage::class.java))
+            finish()
+        }
+    }
     }
 }
