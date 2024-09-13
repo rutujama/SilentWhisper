0bf4044911ab854e74a35b1799815007f3bd6c6f
Yash-2907 <guptayp_1@rknec.edu>
2024-09-13 17:52:13 +0530

    ui

--- app/src/main/java/com/example/silentwhisper/MainActivity.kt
+++ app/src/main/java/com/example/silentwhisper/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.silentwhisper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 //yash push check
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        findViewById<Button>(R.id.shift).setOnClickListener{
+            startActivity(Intent(this@MainActivity,RegisterPage::class.java))
+            finish()
+        }
     }
 }
