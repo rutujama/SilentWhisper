385608f22f7e9899d774788d2067e5b44eb57c40
Yash-2907 <guptayp_1@rknec.edu>
2024-09-15 23:15:57 +0530

    terms and conditions intialized

--- app/src/main/java/com/example/silentwhisper/PermissionsPage.kt
+++ app/src/main/java/com/example/silentwhisper/PermissionsPage.kt
@@ -1,11 +1,24 @@
 package com.example.silentwhisper
 
+import android.content.Intent
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import com.example.silentwhisper.databinding.ActivityPermissionsPageBinding
 
+lateinit var pBinder:ActivityPermissionsPageBinding
 class PermissionsPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_permissions_page)
+        pBinder=ActivityPermissionsPageBinding.inflate(layoutInflater)
+        setContentView(pBinder.root)
+
+        pBinder.acceptbtn.setOnClickListener{
+            startActivity(Intent(this@PermissionsPage,FriendsPage::class.java))
+            finish()
+        }
     }
 }
