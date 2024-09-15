f7b02a0a655507ec47e9caf6a6fef311cbc3ffba
Anushree2023 <anushreethosar@gmail.com>
2024-09-16 00:02:47 +0530

    LogIn Saved Session and LogOut Functionality

--- app/src/main/java/com/example/silentwhisper/FriendsPage.kt
+++ app/src/main/java/com/example/silentwhisper/FriendsPage.kt
@@ -1,11 +1,34 @@
 package com.example.silentwhisper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.silentwhisper.databinding.ActivityFriendsPageBinding
+import com.example.silentwhisper.databinding.ActivityLogInBinding
+import com.example.silentwhisper.databinding.ActivityPermissionsPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class FriendsPage : AppCompatActivity() {
+    lateinit var fbinder: ActivityFriendsPageBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_friends_page)
+        fbinder= ActivityFriendsPageBinding.inflate(layoutInflater)
+        setContentView(fbinder.root)
+
+        auth= FirebaseAuth.getInstance()
+
+        fbinder.logoutbtn.setOnClickListener {
+
+            intent= Intent(this,LogIn::class.java)
+            auth.signOut()
+            startActivity(intent)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            Toast.makeText(this,"You have been Logged Out ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/silentwhisper/PermissionsPage.kt
+++ app/src/main/java/com/example/silentwhisper/PermissionsPage.kt
@@ -6,15 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.text.HtmlCompat
 import com.example.silentwhisper.databinding.ActivityPermissionsPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 lateinit var pBinder:ActivityPermissionsPageBinding
+
 class PermissionsPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         pBinder=ActivityPermissionsPageBinding.inflate(layoutInflater)
         setContentView(pBinder.root)
+
+
 
         pBinder.acceptbtn.setOnClickListener{
             startActivity(Intent(this@PermissionsPage,FriendsPage::class.java))
